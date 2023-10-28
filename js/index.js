cabf49cee7d669f08bedaedb596c1cd9fccdfd9f
Bimsara Walallawita <bwalallawita@gmail.com>
2023-10-28 11:39:27 +0530

    Fetch Products

--- js/index.js
+++ js/index.js
@@ -1,12 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     getProducts();
 });
 
 const getProducts = () => {
-    fetch("http://localhost:8081/products").then((response) => { // if response is successful
-        console.log(response);
-    }).catch((error) => {
-        console.log(error);
-    });
+    fetch("http://localhost:8081/products")
+        .then((response) => { // if response is successful
+            return response.json();
+        }).then((data) => {
+
+            let products = "";
+
+            data.map((product) => { //for each loop
+                
+                
+                    products += `<div class="col-lg-3 col-sm-6 col-12">`; //start new column
+                    products += `<div class="card mb-4">`;
+                    products += `<div class="card-body">`;
+                    products += `<h5 class="card-title">${product.name} <span class="badge bg-secondary">${product.category?.name}</span></h5>`;
+                    products += `<p class="card-text">Rs. ${product.price}</p>`;
+                    products += `<a href="#" class="btn btn-primary">View Product</a>`
+                    products += `</div>`;
+                    products += `</div>`;
+                    products += `</div>`;
+                
+            });
+
+            document.getElementById('products').innerHTML = products;
+
+        }).catch((error) => {
+            console.log(error);
+        });
 }
 
